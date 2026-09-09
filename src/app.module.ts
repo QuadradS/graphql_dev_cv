c6e71c5a291d490bf1d8aae1b86196df6e415980
danil_sitdikov <quadrad4@gmail.com>
2026-09-09 15:49:50 +0600

    - init prisma - init migration - db schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-    }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    // }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
